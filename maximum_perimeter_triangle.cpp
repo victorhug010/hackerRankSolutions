35e1fe29cd822394ff52ec1fefb41a278ca0a0ff
victor hugo <56608136+victorhug010@users.noreply.github.com>
2025-04-18 12:12:06 -0300

    new commits

---
+++ maximum_perimeter_triangle.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'maximumPerimeterTriangle' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY sticks as parameter.
+ */
+
+vector<int> maximumPerimeterTriangle(vector<int> sticks) {
+    bool found = false;
+    vector<int> res;
+    sort(sticks.begin(), sticks.end(), [] (int a, int b){
+       return a > b;
+    });
+
+    for (size_t i = 0;  i < sticks.size(); i++) {
+        size_t indexInit = i + 2;
+
+        if (indexInit >= sticks.size()) {
+            break;
+        }
+        int sum = sticks[indexInit] + sticks[indexInit - 1];
+        if (sum > sticks[indexInit -2]) {
+            found = true;
+            res.push_back(sticks[indexInit]);
+            res.push_back(sticks[indexInit -1]);
+            res.push_back(sticks[indexInit -2]);
+            break;
+        }
+    }
+
+    if (found == false) {
+        return res = {-1};
+    }
+    else {
+        return res;
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string sticks_temp_temp;
+    getline(cin, sticks_temp_temp);
+
+    vector<string> sticks_temp = split(rtrim(sticks_temp_temp));
+
+    vector<int> sticks(n);
+
+    for (int i = 0; i < n; i++) {
+        int sticks_item = stoi(sticks_temp[i]);
+
+        sticks[i] = sticks_item;
+    }
+
+    vector<int> result = maximumPerimeterTriangle(sticks);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
